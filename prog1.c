459f6ab73714aadeeafd666c35a3b227406a1ea8
manoj184 <52406722+manoj184@users.noreply.github.com>
2019-07-02 15:40:23 +0530

    Update prog1.c

---
+++ prog1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i;
+printf("enter the number");
+if(i=0)
+{
+printf("Given number is Zero");
+}
+if(i<0)
+{ 
+printf("Given number is negitive");
+}
+if(i>0)
+{
+printf ("given number is positive");
+}
+scanf("%d",&i);
+}

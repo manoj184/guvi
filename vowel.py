bb4d26da05a66736f2e9c62a7480b67c2920348e
manoj184 <52406722+manoj184@users.noreply.github.com>
2019-07-04 15:47:20 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+manoj=input()
+if(manoj=='a','e','i','o','u'):
+  print("vowel")
+elif(manoj=='b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+  print("consonent")
+else:
+  printf("invalid")

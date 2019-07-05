b1346ceb0bfa5aaf31033d58b2fd6e0d40c9c1df
manoj184 <52406722+manoj184@users.noreply.github.com>
2019-07-05 11:37:11 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,6 @@
-a=input()
+p=input()
 list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-if(a is in list):
+if(p in list):
   print("Alphabet")
 else:
   print("No")

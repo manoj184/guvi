fd0229c9dd3f50ead95bcc58e9b396b9d3a5abd9
manoj184 <52406722+manoj184@users.noreply.github.com>
2019-07-05 11:29:51 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+a=input()
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if(a is in list):
+  print("Alphabet")
+else:
+  print("No")
